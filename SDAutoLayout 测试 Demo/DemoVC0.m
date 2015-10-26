1ccabd15c1dd05b0611cbd4cda9676775683770a
GSD_iOS <gsdios@126.com>
2015-10-26 15:43:59 +0800

    为Demo0 增加具体说明

--- SDAutoLayout 测试 Demo/DemoVC0.m
+++ SDAutoLayout 测试 Demo/DemoVC0.m
@@ -73,6 +73,28 @@
     
 }
 
+
+
+/*
+ Q：这里为什么要在 viewWillLayoutSubviews 里计算宽度？
+ 
+ A：大多情况下的布局需求是不需要在 viewWillLayoutSubviews 里计算宽度，只需要直接一次设置
+    就可以了（例如其他的demo案例）。
+ 
+   1.为什么这个界面要计算view5的宽度？
+ 
+     因为这个界面的布局要求（以view5 和 view6 为例）是：view5 距离屏幕
+    左边10，view6 距离 view5 10， view6 距离屏幕右边10 ，view5 和 view6 宽度相同，这样一来就需要
+    确定 view5 的宽度供 view6 参考，所以要计算下 view5 的宽度。
+ 
+   2.在以上的情形中计算view5宽度理解了，那为什么是要在 viewWillLayoutSubviews 里计算宽度？
+ 
+     如果app需要【同时适配横竖屏】，则在横竖屏幕切换时屏幕的宽高对调，那么就需要再计算一次view5的宽度，而切换屏幕时 在viewWillLayoutSubviews 拿到的self.view的宽度才是正确的（因为viewdidload只是在view初始化完成并加
+    载出来时调用一次，以后切换屏幕不再调用），如果app不需要同时适配
+    横竖屏则不需要此步骤。
+ 
+ 
+ */
 - (void)viewWillLayoutSubviews
 {
     [super viewWillLayoutSubviews];
