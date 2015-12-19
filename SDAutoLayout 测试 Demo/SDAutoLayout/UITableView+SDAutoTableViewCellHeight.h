64fbf1311f889c90447a8a5e98f550694031bee1
GSD_iOS <gsdios@126.com>
2015-12-19 20:34:04 +0800

    支持多cell的高度自适应,更新demo7图片高度按比例缩放

--- SDAutoLayout 测试 Demo/SDAutoLayout/UITableView+SDAutoTableViewCellHeight.h
+++ SDAutoLayout 测试 Demo/SDAutoLayout/UITableView+SDAutoTableViewCellHeight.h
@@ -29,15 +29,26 @@
 
 @interface UITableView (SDAutoTableViewCellHeight)
 
+// ------------------ 单个cell情景下调用以下方法 ------------------
+
+/*
+ *  开启高度自适应，建议在tableview的数据源方法“- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section”中调用此方法，详细用法见demo5
+ */
 - (void)startAutoCellHeightWithCellClass:(Class)cellClass contentViewWidth:(CGFloat)contentViewWidth;
 
+/*
+ *  返回计算出的cell高度
+ *  model:cell的数据模型实例
+ *  keyPath:cell的数据模型属性的属性名字符串（即kvc原理中的key）
+ */
 - (CGFloat)cellHeightForIndexPath:(NSIndexPath *)indexPath model:(id)model keyPath:(NSString *)keyPath;
 
 
-// 多cell情景下调用以下方法
+
+// ------------------ 多cell情景下调用以下方法 ------------------
 
 /*
- * cellClassArray 为所有cell的类组成的数组
+ * cellClassArray 为所有cell的类组成的数组，详细用法见demo7
  */
 - (void)startAutoCellHeightWithCellClasses:(NSArray<Class> *)cellClassArray contentViewWidth:(CGFloat)contentViewWidth;
 
