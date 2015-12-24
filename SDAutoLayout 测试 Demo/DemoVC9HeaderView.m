8c48f49d820274a2155ecdc34d2ef1359edd85e6
GSD_iOS <gsdios@126.com>
2015-12-24 11:39:35 +0800

    增加微信朋友圈demo

--- SDAutoLayout 测试 Demo/DemoVC9HeaderView.m
+++ SDAutoLayout 测试 Demo/DemoVC9HeaderView.m
@@ -6,6 +6,22 @@
 //  Copyright © 2015年 gsd. All rights reserved.
 //
 
+
+/*
+ 
+ *********************************************************************************
+ *                                                                                *
+ * 在您使用此自动布局库的过程中如果出现bug请及时以以下任意一种方式联系我们，我们会及时修复bug并  *
+ * 帮您解决问题。                                                                    *
+ * 持续更新地址: https://github.com/gsdios/SDAutoLayout                              *
+ * Email : gsdios@126.com                                                          *
+ * GitHub: https://github.com/gsdios                                               *
+ * 新浪微博:GSD_iOS                                                                 *
+ *                                                                                *
+ *********************************************************************************
+ 
+ */
+
 #import "DemoVC9HeaderView.h"
 
 #import "UIView+SDAutoLayout.h"
