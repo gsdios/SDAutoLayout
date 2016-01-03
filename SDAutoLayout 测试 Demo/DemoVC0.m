a4598718463ce43085039d3d50feb56186b6c827
GSD_iOS <gsdios@126.com>
2016-01-04 00:00:49 +0800

    增加1.自动布局动画；2.任何类型对象都可以实现一行代码搞定cell高度自适应；3.增加文档注释。

--- SDAutoLayout 测试 Demo/DemoVC0.m
+++ SDAutoLayout 测试 Demo/DemoVC0.m
@@ -23,41 +23,41 @@
 
 #import "DemoVC0.h"
 
+#define kTimeInterval 0.8
+
 @implementation DemoVC0
+{
+    NSTimer *_timer;
+    CGFloat _widthRatio;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     
+    _widthRatio = 0.4;
+    
+    _timer = [NSTimer scheduledTimerWithTimeInterval:kTimeInterval target:self selector:@selector(animation) userInfo:nil repeats:YES];
+    
     self.edgesForExtendedLayout = UIRectEdgeNone;
     
     self.view0.sd_layout
-    .leftSpaceToView(self.view, 10)
+    .leftSpaceToView(self.view, 20)
     .topSpaceToView(self.view,80)
     .heightIs(130)
-    .widthRatioToView(self.view, 0.4);
+    .widthRatioToView(self.view, _widthRatio);
     
     self.view1.sd_layout
     .leftSpaceToView(self.view0, 10)
     .topEqualToView(self.view0)
-    .heightIs(60);
-    
-    self.view0.sd_layout
-    .leftSpaceToView(self.view, 10)
-    .topSpaceToView(self.view,80)
-    .heightIs(130)
-    .widthRatioToView(self.view, 0.4);
-    
-    self.view1.sd_layout
-    .leftSpaceToView(self.view0, 10)
-    .topEqualToView(self.view0)
-    .heightIs(60);
+    .heightIs(60)
+    .widthRatioToView(self.view0, 0.5);
     
     self.view2.sd_layout
     .leftSpaceToView(self.view1, 10)
     .topEqualToView(self.view1)
     .heightRatioToView(self.view1, 1)
-    .widthRatioToView(self.view1, 1);
+    .widthIs(50);
     
     self.view3.sd_layout
     .leftEqualToView(self.view1)
@@ -69,54 +69,51 @@
     .leftEqualToView(self.view2)
     .topEqualToView(self.view3)
     .heightRatioToView(self.view3, 1)
-    .widthRatioToView(self.view1, 1);
+    .widthRatioToView(self.view2, 1);
     
     
+    [self.view0 addSubview:self.view5];
     self.view5.sd_layout
-    .topSpaceToView(self.view0, 20)
-    .leftEqualToView(self.view0)
-    .bottomSpaceToView(self.view, 30);
-    
-    self.view6.sd_layout
-    .leftSpaceToView(self.view5, 10)
-    .topEqualToView(self.view5)
-    .heightRatioToView(self.view5, 1)
-    .widthRatioToView(self.view5, 1);
+    .centerYEqualToView(self.view0)
+    .rightSpaceToView(self.view0, 10)
+    .widthRatioToView(self.view0, 0.5)
+    .heightIs(20);
+
+//    self.view6.sd_layout
+//    .centerXEqualToView(self.view)
+//    .topSpaceToView(self.view0, 50)
+//    .heightRatioToView(self.view, 1)
+//    .widthRatioToView(self.view5, 1);
+
 }
 
 
 
-/*
- Q：这里为什么要在 viewWillLayoutSubviews 里计算宽度？
- 
- A：大多情况下的布局需求是不需要在 viewWillLayoutSubviews 里计算宽度，只需要直接一次设置
-    就可以了（例如其他的demo案例）。
- 
-   1.为什么这个界面要计算view5的宽度？
- 
-     因为这个界面的布局要求（以view5 和 view6 为例）是：view5 距离屏幕
-    左边10，view6 距离 view5 10， view6 距离屏幕右边10 ，view5 和 view6 宽度相同，这样一来就需要
-    确定 view5 的宽度供 view6 参考，所以要计算下 view5 的宽度。
- 
-   2.在以上的情形中计算view5宽度理解了，那为什么是要在 viewWillLayoutSubviews 里计算宽度？
- 
-     如果app需要【同时适配横竖屏】，则在横竖屏幕切换时屏幕的宽高对调，那么就需要再计算一次view5的宽度，而切换屏幕时 在viewWillLayoutSubviews 拿到的self.view的宽度才是正确的（因为viewdidload只是在view初始化完成并加
-    载出来时调用一次，以后切换屏幕不再调用），如果app不需要同时适配
-    横竖屏则不需要此步骤。
- 
- 
- */
-- (void)viewWillLayoutSubviews
+- (void)animation
 {
-    [super viewWillLayoutSubviews];
+    if (_widthRatio == 0.4) {
+        _widthRatio = 0.1;
+    } else {
+        _widthRatio = 0.4;
+    }
+    [UIView animateWithDuration:0.8 animations:^{
+        self.view0.sd_layout
+        .widthRatioToView(self.view, _widthRatio);
+        [self.view layoutSubviews];
+        [self.view0 layoutSubviews];
+    }];
     
-    CGFloat view1W = (self.view.width * 0.6 - 10 * 4) / 2;
-    self.view1.sd_layout.widthIs(view1W);
     
-    CGFloat view5W = (self.view.width - 30) / 2;
-    self.view5.sd_layout.widthIs(view5W);
-}
+    /*
+     
+     SDAutoLayout（新建QQ交流群：497140713）
+     github:https://github.com/gsdios/SDAutoLayout
+     ☆☆ SDAutoLayout 视频教程：http://www.letv.com/ptv/vplay/24038772.html ☆☆
+     一行代码搞定自动布局！致力于做最简单易用的Autolayout库。The most easy way for autolayout.
+     
+     */
 
+}
 
 @end
 
