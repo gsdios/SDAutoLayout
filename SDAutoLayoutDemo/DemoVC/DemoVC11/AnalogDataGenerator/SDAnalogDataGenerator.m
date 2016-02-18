7856dc8a7b299a94f481cda0302a1493befbbd7b
GSD_iOS <gsdios@126.com>
2016-02-18 10:46:57 +0800

    增加微信聊天界面demo
    增加微信聊天界面demo

--- SDAutoLayoutDemo/DemoVC/DemoVC11/AnalogDataGenerator/SDAnalogDataGenerator.m
+++ SDAutoLayoutDemo/DemoVC/DemoVC11/AnalogDataGenerator/SDAnalogDataGenerator.m
@@ -6,6 +6,25 @@
 //  Copyright © 2016年 GSD. All rights reserved.
 //
 
+
+/*
+ 
+ *********************************************************************************
+ *                                                                                *
+ * 在您使用此自动布局库的过程中如果出现bug请及时以以下任意一种方式联系我们，我们会及时修复bug并  *
+ * 帮您解决问题。                                                                    *
+ * 持续更新地址: https://github.com/gsdios/SDAutoLayout                              *
+ * Email : gsdios@126.com                                                          *
+ * GitHub: https://github.com/gsdios                                               *
+ * 新浪微博:GSD_iOS                                                                 *
+ * QQ交流群：519489682（已满）497140713                                              *
+ *********************************************************************************
+ 
+ */
+
+
+
+
 #import "SDAnalogDataGenerator.h"
 
 static NSArray *namesArray;
