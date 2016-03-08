838263a6505a8ed841a928ff7f03b5b5de0a86f5
GSD_iOS <gsdios@126.com>
2016-03-08 15:43:21 +0800

    更新demo1

--- SDAutoLayoutDemo/DemoVC/DemoVC1/DemoVC1.m
+++ SDAutoLayoutDemo/DemoVC/DemoVC1/DemoVC1.m
@@ -30,75 +30,47 @@
     [super viewDidLoad];
     
     
-    // 暂时用view替代展示cell效果
+    /*  
+     
+     设置view1高度根据子view而自适应(在view1中加入两个子view(testLabel和testView)，然后设置view1高度根据子view内容自适应)
+     */
     
-    self.view0.sd_layout
-    .leftSpaceToView(self.view, 10)
-    .topSpaceToView(self.view, 80)
-    .widthIs(50)
-    .heightIs(50);
     
-    // view1使用高度根据子view内容自适应，所以不需要设置高度，而是在后面设置“[self.view1 setupAutoHeightWithBottomView:testView bottomMargin:10];”实现高度根据内容自适应
-    self.view1.sd_layout
-    .leftSpaceToView(self.view0, 10)
-    .topEqualToView(self.view0)
-    .rightSpaceToView(self.view, 30);
-    
-    self.view2.sd_layout
-    .rightSpaceToView(self.view, 10)
-    .topSpaceToView(self.view1, 20)
-    .widthRatioToView(self.view0, 1)
-    .heightRatioToView(self.view0, 1);
-    
-    self.view3.sd_layout
-    .rightSpaceToView(self.view2, 10)
-    .leftSpaceToView(self.view, 30)
-    .heightRatioToView(self.view, 0.17)
-    .topEqualToView(self.view2);
-    
-    self.view4.sd_layout
-    .leftEqualToView(self.view0)
-    .heightRatioToView(self.view0, 1)
-    .widthRatioToView(self.view0, 1)
-    .topSpaceToView(self.view3, 20);
-    
-    self.view5.sd_layout
-    .leftEqualToView(self.view1)
-    .rightEqualToView(self.view1)
-    .bottomSpaceToView(self.view, 20)
-    .topEqualToView(self.view4);
 
-//  ============================================================
+    UILabel *testLabel_subview1 = [UILabel new]; // 初始化子view1
+    testLabel_subview1.text = @"这个紫色的label会根据这些文字内容高度自适应；而这个灰色的父view会根据紫色的label和橙色的view具体情况实现高度自适应。\nGot it! OH YAEH!";
+    testLabel_subview1.backgroundColor = [UIColor purpleColor];
     
-    // 在view1中加入两个子view，然后设置view1高度根据子view内容自适应
+    UIView *testView_subview2 = [UIView new];    // 初始化子view2
+    testView_subview2.backgroundColor = [UIColor orangeColor];
+    
+    // 将子view添加进父view
+    [self.view1 sd_addSubviews:@[testLabel_subview1, testView_subview2]];
     
     
-    UILabel *testLabel = [UILabel new];
-    UIView *testView = [UIView new];
-    [self.view1 addSubview:testLabel];
-    [self.view1 addSubview:testView];
-    
-    testLabel.backgroundColor = [UIColor purpleColor];
-    testView.backgroundColor = [UIColor orangeColor];
-    
-    testLabel.text = @"edflgjl;ijf;iljd;lij;lfk;lknsdfhfg;ljhl;sfdj;oigfj;jgf;lfgjrlkfewiorrgi";
-    
-    testLabel.sd_layout
+    testLabel_subview1.sd_layout
     .leftSpaceToView(self.view1, 10)
     .rightSpaceToView(self.view1, 10)
     .topSpaceToView(self.view1, 10)
     .autoHeightRatio(0); // 设置文本内容自适应，如果这里的参数为大于0的数值则会以此数值作为view的高宽比设置view的高度
     
-    testView.sd_layout
-    .topSpaceToView(testLabel, 10)
-    .widthRatioToView(testLabel, 1)
+    testView_subview2.sd_layout
+    .topSpaceToView(testLabel_subview1, 10)
+    .widthRatioToView(testLabel_subview1, 1)
     .heightIs(30)
-    .leftEqualToView(testLabel);
+    .leftEqualToView(testLabel_subview1);
+    
+    
+    // view1使用高度根据子view内容自适应，所以不需要设置高度，而是设置“[self.view1 setupAutoHeightWithBottomView:testView bottomMargin:10];”实现高度根据内容自适应
+    self.view1.sd_layout
+    .leftSpaceToView(self.view, 10)
+    .topSpaceToView(self.view, 80)
+    .rightSpaceToView(self.view, 10);
     
     // 设置view1高度根据子其内容自适应
-    [self.view1 setupAutoHeightWithBottomView:testView bottomMargin:10];
+    [self.view1 setupAutoHeightWithBottomView:testView_subview2 bottomMargin:10];
     
-//  ============================================================
+    
 }
 
 @end
