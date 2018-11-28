2e9ff3c261059cfe5dc074e38a132e73e4d29ad1
GSD_iOS <gsdios@126.com>
2018-11-28 12:03:25 +0800

    merge swift extension

--- SDAutoLayoutDemo/SwiftExtension/SDAutolayout+extension.swift
+++ SDAutoLayoutDemo/SwiftExtension/SDAutolayout+extension.swift
@@ -20,136 +20,136 @@ extension SDAutoLayoutModel {
     /* 设置距离其它view的间距 */
     @discardableResult
     public func topTo(_ view: UIView, _ space: CGFloat) -> SDAutoLayoutModel {
-        return topSpaceToView(view, space)!
+        return topSpaceToView(view, space)
     }
     
     @discardableResult
     public func bottomTo(_ view: UIView, _ space: CGFloat) -> SDAutoLayoutModel {
-        return bottomSpaceToView(view, space)!
+        return bottomSpaceToView(view, space)
     }
     
     @discardableResult
     public func leftTo(_ view: UIView, _ space: CGFloat) -> SDAutoLayoutModel {
-        return leftSpaceToView(view, space)!
+        return leftSpaceToView(view, space)
     }
     
     @discardableResult
     public func rightTo(_ view: UIView, _ space: CGFloat) -> SDAutoLayoutModel {
-        return rightSpaceToView(view, space)!
+        return rightSpaceToView(view, space)
     }
     
     /* 设置x、y、width、height、centerX、centerY 值 */
     @discardableResult
     public func x(is value: CGFloat) -> SDAutoLayoutModel {
-        return xIs(value)!
+        return xIs(value)
     }
     
     @discardableResult
     public func y(is value: CGFloat) -> SDAutoLayoutModel {
-        return yIs(value)!
+        return yIs(value)
     }
     
     @discardableResult
     public func width(is value: CGFloat) -> SDAutoLayoutModel {
-        return widthIs(value)!
+        return widthIs(value)
     }
     
     @discardableResult
     public func height(is value: CGFloat) -> SDAutoLayoutModel {
-        return heightIs(value)!
+        return heightIs(value)
     }
     
     @discardableResult
     public func centerX(is value: CGFloat) -> SDAutoLayoutModel {
-        return centerXIs(value)!
+        return centerXIs(value)
     }
     
     @discardableResult
     public func centerY(is value: CGFloat) -> SDAutoLayoutModel {
-        return centerYIs(value)!
+        return centerYIs(value)
     }
     
     /* 设置最大宽度和高度、最小宽度和高度 */
     @discardableResult
     public func maxWidth(_ value: CGFloat) -> SDAutoLayoutModel {
-        return maxWidthIs(value)!
+        return maxWidthIs(value)
     }
     
     @discardableResult
     public func maxHeight(_ value: CGFloat) -> SDAutoLayoutModel {
-        return maxHeightIs(value)!
+        return maxHeightIs(value)
     }
     
     @discardableResult
     public func minWidth(_ value: CGFloat) -> SDAutoLayoutModel {
-        return minWidthIs(value)!
+        return minWidthIs(value)
     }
     
     @discardableResult
     public func minHeight(_ value: CGFloat) -> SDAutoLayoutModel {
-        return minHeightIs(value)!
+        return minHeightIs(value)
     }
     
     /* 设置和某个参照view的边距相同 */
     @discardableResult
     public func leftEqualTo(_ view: UIView) -> SDAutoLayoutModel {
-        return leftEqualToView(view)!
+        return leftEqualToView(view)
     }
     
     @discardableResult
     public func rightEqualTo(_ view: UIView) -> SDAutoLayoutModel {
-        return rightEqualToView(view)!
+        return rightEqualToView(view)
     }
     
     @discardableResult
     public func topEqualTo(_ view: UIView) -> SDAutoLayoutModel {
-        return topEqualToView(view)!
+        return topEqualToView(view)
     }
     
     @discardableResult
     public func bottomEqualTo(_ view: UIView) -> SDAutoLayoutModel {
-        return bottomEqualToView(view)!
+        return bottomEqualToView(view)
     }
     
     @discardableResult
     public func centerXEqualTo(_ view: UIView) -> SDAutoLayoutModel {
-        return centerXEqualToView(view)!
+        return centerXEqualToView(view)
     }
     
     @discardableResult
     public func centerYEqualTo(_ view: UIView) -> SDAutoLayoutModel {
-        return centerYEqualToView(view)!
+        return centerYEqualToView(view)
     }
     
     /*  设置宽度或者高度等于参照view的多少倍 */
     @discardableResult
     public func widthRatioTo(_ view: UIView, _ value: CGFloat) -> SDAutoLayoutModel {
-        return widthRatioToView(view, value)!
+        return widthRatioToView(view, value)
     }
     
     @discardableResult
     public func heightRatioTo(_ view: UIView, _ value: CGFloat) -> SDAutoLayoutModel {
-        return heightRatioToView(view, value)!
+        return heightRatioToView(view, value)
     }
     
     @discardableResult
     public func autoHeight(_ ratio: CGFloat) -> SDAutoLayoutModel {
-        return autoHeightRatio(ratio)!
+        return autoHeightRatio(ratio)
     }
     
     @discardableResult
     public func autoWidth(_ ratio: CGFloat) -> SDAutoLayoutModel {
-        return autoWidthRatio(ratio)!
+        return autoWidthRatio(ratio)
     }
     
     @discardableResult
     public func isWidthEqualToHeight() -> SDAutoLayoutModel {
-        return widthEqualToHeight()!
+        return widthEqualToHeight()
     }
     
     @discardableResult
     public func isHeightEqualToWidth() -> SDAutoLayoutModel {
-        return heightEqualToWidth()!
+        return heightEqualToWidth()
     }
     
     /* 填充父view(快捷方法) */
@@ -159,7 +159,7 @@ extension SDAutoLayoutModel {
     
     @discardableResult
     public func offsetIs(_ value: CGFloat) -> SDAutoLayoutModel {
-        return offset(value)!
+        return offset(value)
     }
     
     
